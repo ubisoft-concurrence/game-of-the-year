f4494d7fb5e346cc629633f810f5403a64c33373
ntmanua <manuarii.natanni@gmail.com>
2023-06-26 10:11:56 +0200

    get character by id

--- backend/app/database.js
+++ backend/app/database.js
@@ -8,8 +8,15 @@ const pool = mysql.createPool({
     database: 'game',
 }).promise()
 
-
+//Récupèrer la liste des personnages
 export async function getCharacters() {
     const result = await pool.query("SELECT * FROM characters");
+    return result[0];
+  }
+
+//Récupèrer 1 personnage selon son id (URL sensible à la casse)
+export async function getCharacter(id) {
+    const result = await pool.query(
+        `SELECT * FROM characters WHERE character_id = ?`, [id])
     return result[0];
   }
