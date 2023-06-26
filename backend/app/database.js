c4f56ffed18bc2e529c3aa3e9815c60881cfffe8
ntmanua <manuarii.natanni@gmail.com>
2023-06-26 13:34:59 +0200

    format update

--- backend/app/database.js
+++ backend/app/database.js
@@ -10,16 +10,18 @@ const pool = mysql.createPool({
 
 //Récupérer la liste des personnages (nom, classe, level)
 export async function getCharacters() {
-    const result = await pool.query(
-        `SELECT character_name, class_name, character_level 
-         FROM characters JOIN classes ON classes.class_id = characters.class_id;`)
+    const result = await pool.query(`
+        SELECT character_name, class_name, character_level 
+        FROM characters JOIN classes ON classes.class_id = characters.class_id
+        `)
     return result[0];
   }
 
 //Récupérer 1 personnage selon son id (URL sensible à la casse)
 export async function getCharacter(id) {
-    const result = await pool.query(
-        `SELECT * FROM characters WHERE character_id = ?`, [id])
+    const result = await pool.query(`
+    SELECT * FROM characters WHERE character_id = ?
+    `, [id])
     return result[0];
 }
 
@@ -27,7 +29,8 @@ export async function getCharacter(id) {
 export async function createCharacter(character_name, skin, class_id) {
     const result = await pool.query(`
     INSERT INTO characters (character_name, skin, class_id)
-    VALUES (?, ?)`, [character_name, skin, class_id])
+    VALUES (?, ?)
+    `, [character_name, skin, class_id])
     return result
 }
 
@@ -35,40 +38,47 @@ export async function createCharacter(character_name, skin, class_id) {
 export async function createVehicule(vehicule_name, buff, nerf) {
     await pool.query(`
     INSERT INTO vehicles (vehicle_name, buff, nerf)
-    VALUES (?, ?, ?)`, [vehicule_name, buff, nerf])
+    VALUES (?, ?, ?)
+    `, [vehicule_name, buff, nerf])
 }
 
 //Récupérer la liste des vehicules
 export async function getVehicles() {
-    const vehicles = await pool.query(`SELECT * FROM vehicles`);
+    const vehicles = await pool.query(`
+    SELECT * FROM vehicles
+    `)
     return vehicles[0];
 }
 
 //Récupérer le classement
 export async function getRanking() {
-    const ranking = await pool.query(
-        `SELECT character_name, character_level 
-        FROM characters ORDER BY character_level DESC`
-    );
+    const ranking = await pool.query(`
+        SELECT character_name, character_level 
+        FROM characters ORDER BY character_level DESC
+    `)
 }
 
 //Récupérer l'historique des batailles
 export async function getHistoric(){
-    const hisotric = await pool.query(
-        `SELECT battles.battle_id, character_name, result 
-         FROM characters 
-         JOIN battles_characters ON (characters.character_id = battles_characters.character_id) 
-         JOIN battles ON (battles.battle_id = battles_characters.battle_id)`
-    )
+    const hisotric = await pool.query(`
+        SELECT battles.battle_id, character_name, result 
+        FROM characters 
+        JOIN battles_characters 
+        ON (characters.character_id = battles_characters.character_id) 
+        JOIN battles 
+        ON (battles.battle_id = battles_characters.battle_id)
+    `)
 }
 
-//Monter de level (winners liste d'objets JS contenant character_name et character_level) )
+//Monter de level (winners liste d'objets JS contenant character_name et character_level)
 export async function levelUp(winners) {
     for (let character of winners) {
-        await pool.query(
-            `UPDATE characters SET character_level = ? WHERE character_name = ?`,
-            [character.character_level, character.character_name]
-        );
+        await pool.query(`
+            UPDATE characters 
+            SET character_level = ? 
+            WHERE character_name = ?
+            `, [character.character_level, character.character_name]
+        )
     }
 }
 
