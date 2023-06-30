0ab8da14b74db59c228f437b825b5f48b8acad0a
ntmanua <manuarii.natanni@gmail.com>
2023-06-30 14:37:19 +0200

    Get gang choice data is OK

--- front-end/JS/battle-config-script.js
+++ front-end/JS/battle-config-script.js
@@ -1,52 +1,79 @@
+const output = document.getElementById('ListPlayer');
+fetch('http://localhost:3000/characters')
+  .then(response => response.json())
+  .then(characters => {
+    console.log(characters)
+    let i = 0
+    characters.forEach(element => {
+      i++;
+      console.log(element)
+      output.innerHTML += `<div class='player divbackground textDiv' draggable='true' ondragstart='drag(event)' id='drag${i}'>`
+        + `<p class="pseudo">` + element.character_name + `</p><p>` + element.class_name + `</p><p> LV:` + element.character_level + `</p>` + `</div>`;
+    });
+  })
+
+const output2 = document.getElementById('buschoicesave');
+const output3 = document.getElementById('buschoicesave2');
+fetch('http://localhost:3000/vehicles')
+  .then(response => response.json())
+  .then(vehicles => {
+    console.log(vehicles)
+    let x = 0
+    vehicles.forEach(element => {
+      x++;
+      console.log(element)
+      output2.innerHTML += `<option value="bus${x}">` + element.vehicle_name + `</div>`;
+      output3.innerHTML += `<option value="bus${x}">` + element.vehicle_name + `</div>`;
+    });
+  })
+
+
 const divGang1 = document.getElementById("Gang1");
 const divGang2 = document.getElementById("Gang2");
-let gang1 = [];
-let gang2 = [];
 
-function drop(event) {
-  event.preventDefault();
-  let targetDiv = event.currentTarget;
-  let pseudo = event.dataTransfer.getData("text");
-  let pElement = document.createElement("p");
-  pElement.textContent = pseudo;
-
-  if (targetDiv.id === "Gang1") {
-    gang1.push(pseudo);
-    divGang1.appendChild(pElement);
-  } else if (targetDiv.id === "Gang2") {
-    gang2.push(pseudo);
-    divGang2.appendChild(pElement);
-  }
-
-  let gang1 = [];
-  let gang2 = [];
+function allowDrop(ev) {
+  ev.preventDefault();
 }
 
-  let output = document.getElementById('ListPlayer');
-  fetch('http://localhost:3000/characters')
-    .then(response => response.json())
-    .then(characters => {
-      console.log(characters)
-      let i = 0
-      characters.forEach(element => {
-        i++;
-        console.log(element)
-        output.innerHTML += `<div class='divbackground textDiv' draggable='true' ondragstart='drag(event)' id='drag${i}'>`
-          + `|` + element.character_name + `|` + element.class_name + `| LV:` + element.character_level + `|` + `</div>`;
-      });
-    })
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
 
-  let output2 = document.getElementById('buschoicesave');
-  let output3 = document.getElementById('buschoicesave2');
-  fetch('http://localhost:3000/vehicles')
-    .then(response => response.json())
-    .then(vehicles => {
-      console.log(vehicles)
-      let x = 0
-      vehicles.forEach(element => {
-        x++;
-        console.log(element)
-        output2.innerHTML += `<option value="bus${x}">` + element.vehicle_name + `</div>`;
-        output3.innerHTML += `<option value="bus${x}">` + element.vehicle_name + `</div>`;
-      });
-    })
+function drop(even) {   
+  even.preventDefault();
+  var fetchData = even.dataTransfer.getData("text");
+  even.currentTarget.appendChild(document.getElementById(fetchData));
+}
+
+
+const busGang1Select = document.getElementById("buschoicesave");
+const busGang2Select = document.getElementById("buschoicesave2");
+const startBtn = document.getElementById("btn-start");
+
+startBtn.addEventListener("click", () => {
+  const busGang1 = busGang1Select.options[busGang1Select.selectedIndex].textContent;
+  const busGang2 = busGang2Select.options[busGang2Select.selectedIndex].textContent;
+  
+  let gang1 = [];
+  let gang2 = [];
+
+  gang1.push(busGang1);
+  gang2.push(busGang2);
+
+  let fighters1 = document.querySelectorAll("#Gang1 .player");
+  let fighters2 = document.querySelectorAll("#Gang2 .player");
+  
+  fighters1.forEach(function(fighter) {
+    let pseudo = fighter.querySelector(".pseudo").textContent;
+    gang1.push(pseudo);
+  });
+
+  fighters2.forEach(function(fighter) {
+    let pseudo = fighter.querySelector(".pseudo").textContent;
+    gang2.push(pseudo);
+  });
+
+  console.log(gang1);
+  console.log(gang2);
+});
+
