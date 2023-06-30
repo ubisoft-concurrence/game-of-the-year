c97f96aeb9aa06fa778e5a6b2e25f2c6a8cdbf2f
ntmanua <manuarii.natanni@gmail.com>
2023-06-30 11:37:03 +0200

    try to get drag div content

--- front-end/JS/battle-config-script.js
+++ front-end/JS/battle-config-script.js
@@ -1,16 +1,31 @@
-function allowDrop(ev) {
-  ev.preventDefault();
+const divGang1 = document.getElementById("Gang1");
+const divGang2 = document.getElementById("Gang2");
+let gang1 = [];
+let gang2 = [];
+
+function drop(event) {
+  event.preventDefault();
+  let targetDiv = event.currentTarget;
+  let pseudo = event.dataTransfer.getData("text");
+  let pElement = document.createElement("p");
+  pElement.textContent = pseudo;
+
+  if (targetDiv.id === "Gang1") {
+    gang1.push(pseudo);
+    divGang1.appendChild(pElement);
+  } else if (targetDiv.id === "Gang2") {
+    gang2.push(pseudo);
+    divGang2.appendChild(pElement);
+  }
+
+  console.log(gang1);
+  console.log(gang2);
 }
 
-function drag(ev) {
-  ev.dataTransfer.setData("text", ev.target.id);
+function drag(event) {
+  event.dataTransfer.setData("text", event.target);
 }
 
-function drop(even) {   
-  even.preventDefault();
-  var fetchData = even.dataTransfer.getData("text");
-  even.currentTarget.appendChild(document.getElementById(fetchData));
+function allowDrop(event) {
+  event.preventDefault();
 }
-
-let gang1=[];
-let gang2=[];
