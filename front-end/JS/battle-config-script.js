fe68835de3271d2f2ad3cfe859f8ae039529c943
JunkBezoul <alex.buffet7839@gmail.com>
2023-06-30 11:26:30 +0200

    fetchchara

--- front-end/JS/battle-config-script.js
+++ front-end/JS/battle-config-script.js
@@ -14,3 +14,18 @@ function drop(even) {
 
 let gang1=[];
 let gang2=[];
+
+  let output = document.getElementById('ListPlayer');
+     fetch('http://localhost:3000/characters')
+     .then(response => response.json())
+     .then(characters => {
+      console.log(characters)
+      let i = 0
+      characters.forEach(element => {
+        i++;
+        console.log(element)
+        output.innerHTML += `<div class='divbackground textDiv' draggable='true' ondragstart='drag(event)' id='drag${i}'>` 
+        + `|` + element.character_name  + `|` + element.class_name  + `| LV:` + element.character_level  + `|` + `</div>`;
+     });
+      
+     })
