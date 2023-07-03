bc49d713bd83db221f2b72060c0cad0aa28cd305
ntmanua <manuarii.natanni@gmail.com>
2023-07-03 14:22:52 +0200

    clean code

--- front-end/JS/turn-based-battle.js
+++ front-end/JS/turn-based-battle.js
@@ -1,103 +1,11 @@
 const getFighters = document.querySelector('#getFighters')
 const busContainer = document.querySelector('.busContainer')
-
-// const greenBar = document.querySelectorAll('.greenbar')
-// let push = 75;
-// pointVie.innerHTML = push;
-
-
-//             function pushVie(){
-//                 push -= 15; // Réduire la valeur de push de 10
-
-//                 if (push < 0) {
-//                     push = 0; // Assurer que la valeur de push ne devienne pas négative
-//                 }
-//                 let table = [];
-//                 pointVie[i].innerHTML = push
-//                 greenBar[i].style.width =  push * 1.33 + '%';
-//                 table.push(push);
-
-//                 if(push == 0){
-//                     img[i].style.backgroundPositionX = '50%'
-//                 }
-//             }
-//                 setInterval(pushVie, 1000,)
-
-
-
-
-// console.log(pointVie.textContent);
-
-// Définition des joueurs
-
-// const joueur1 = {
-//     nom: "Joueur 1",
-//     pointsDeVie: 100,
-//     attaqueMin: 8,
-//     attaqueMax: 15
-// };
-
-// const joueur2 = {
-//     nom: "Joueur 2",
-//     pointsDeVie: 100,
-//     attaqueMin: 8,
-//     attaqueMax: 15
-// };
-
-// // Fonction pour générer un nombre aléatoire entre min et max
-// function genererNombreAleatoire(min, max) {
-//     return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-
-// // Fonction pour effectuer une attaque d'un joueur à un autre
-
-// function attaquer(attaquant, defenseur) {
-//     const pointsDeDegats = genererNombreAleatoire(attaquant.attaqueMin, attaquant.attaqueMax);
-//     defenseur.pointsDeVie -= pointsDeDegats;
-//     console.log(`${attaquant.nom} attaque ${defenseur.nom} et lui inflige ${pointsDeDegats} points de dégâts.`);
-// }
-
-// // Fonction pour vérifier si un joueur est en vie
-// function estEnVie(joueur) {
-//     return joueur.pointsDeVie > 0;
-// }
-
-// // Fonction pour afficher l'état des joueurs
-// function afficherEtatDesJoueurs() {
-//     console.log("--------------------");
-//     console.log(`${joueur1.nom}: ${joueur1.pointsDeVie} health_point`);
-//     console.log(`${joueur2.nom}: ${joueur2.pointsDeVie} health_point`);
-//     console.log("--------------------");
-// }
-
-// // Boucle principale du jeu
-// while (estEnVie(joueur1) && estEnVie(joueur2)) {
-//     // Tour du joueur 1
-//     attaquer(joueur1, joueur2)
-//     // attaquer(joueur1, joueur2);
-//     if (!estEnVie(joueur2)) {
-//       break; // Si le joueur 2 est mort, sortir de la boucle
-//     }
-
-//     // Tour du joueur 2
-//     attaquer(joueur2, joueur1);
-//   }
-
-//   // Afficher le résultat du jeu
-//   if (estEnVie(joueur1)) {
-//     console.log(`${joueur1.nom} a gagné !`);
-//   } else if (estEnVie(joueur2)) {
-//     console.log(`${joueur2.nom} a gagné !`);
-//   } else {
-//     console.log("Match nul !");
-//   }
 const busTeamOne = document.querySelector('#busTeamOne');
 const busTeamTwo = document.querySelector('#busTeamTwo');
 
 fetch("http://localhost:3000/battlesettings")
     .then(res => res.json())
     .then(data => {
-        console.log(data);
         let teamOne;
         let teamTwo;
 
@@ -117,17 +25,13 @@ fetch("http://localhost:3000/battlesettings")
             divTeamTwo = document.createElement('div')
             divTeamTwo.classList.add('teamTwo')
         }
-        
-        console.log(teamOne);
-        console.log(teamTwo);
-
+    
         const allFighters = teamOne.concat(teamTwo);
         const idRound = document.querySelector('.idRound')
 
-
         let increment = 0;
         teamOne.forEach(elements => {
-            console.log(elements);
+
             increment++;
             const divGrid = document.createElement('div');
 
@@ -163,7 +67,6 @@ fetch("http://localhost:3000/battlesettings")
 
         })
         teamTwo.forEach(elements => {
-            console.log(elements);
             increment++;
             const divGrid = document.createElement('div');
 
@@ -216,8 +119,6 @@ fetch("http://localhost:3000/battlesettings")
         for (let figther of allFighters) {
             pvMax.push(figther.health_point)
         }
-        console.log(pvMax)
-
 
         // function attack
         const status = document.querySelector('.infosRound')
